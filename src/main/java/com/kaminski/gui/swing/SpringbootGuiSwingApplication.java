73893edcfbf15d6ea9913989265463ca95a23b9d
Willian Kaminski dos Santos <developer.williankaminski@gmail.com>
2024-03-31 10:32:56 -0300

    first commit

---
+++ src/main/java/com/kaminski/gui/swing/SpringbootGuiSwingApplication.java
@@ -0,0 +1,13 @@
+package com.kaminski.gui.swing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGuiSwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGuiSwingApplication.class, args);
+	}
+
+}
