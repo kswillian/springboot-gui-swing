1ceb5abddbc4d76448530bf22cec5859ae865385
Willian Kaminski dos Santos <developer.williankaminski@gmail.com>
2024-04-01 11:35:28 -0300

    add main jManuBar

--- src/main/java/com/kaminski/gui/swing/view/JFrameMainView.java
+++ src/main/java/com/kaminski/gui/swing/view/JFrameMainView.java
@@ -1,14 +1,17 @@
 package com.kaminski.gui.swing.view;
 
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import java.awt.*;
 
 @Component
+@RequiredArgsConstructor
 public class JFrameMainView extends JFrame {
 
+    private final JMenuBarMainView jMenuBarMainView;
     private static final String APPLICATION_NAME = "Spring + Swing";
 
     @PostConstruct
@@ -25,6 +28,7 @@ public class JFrameMainView extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setJMenuBar(jMenuBarMainView);
         setVisible(true);
     }
 }
---
+++ src/main/java/com/kaminski/gui/swing/view/JMenuBarMainView.java
@@ -0,0 +1,39 @@
+package com.kaminski.gui.swing.view;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+@Component
+public class JMenuBarMainView extends JMenuBar {
+
+    @PostConstruct
+    public JMenuBar init(){
+        create();
+        return this;
+    }
+
+    private void create(){
+
+        var jMenuFile = new JMenu("Arquivo");
+        var jMenuEdit = new JMenu("Editar");
+        var jMenuSystem = new JMenu("Sistema");
+        var jMenuHelp = new JMenu("Ajuda");
+
+        var jMenuItemExit = new JMenuItem("Sair");
+        var jMenuItemSystemDetails = new JMenuItem("Detalhes");
+        var jMenuItemDeviceDetails = new JMenuItem("Meu Dispositivo");
+
+        jMenuFile.add(jMenuItemExit);
+        jMenuSystem.add(jMenuItemSystemDetails);
+        jMenuSystem.add(jMenuItemDeviceDetails);
+
+        add(jMenuFile);
+        add(jMenuEdit);
+        add(jMenuSystem);
+        add(jMenuHelp);
+    }
+}
